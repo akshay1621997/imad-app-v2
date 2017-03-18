b31f3d84c90bc74556b4925e8f339eb871078fc2
akshay1621997 <achu1621997@gmail.com>
2017-03-18 22:34:23 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -18,15 +18,6 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
  
-function hash (input,salt){
-    var hashed=crypto.pbkdf2Sync(input,salt,10000,512,'sha512');
-    return hashed.toString('hex');
-}
-
-app.get('hash/:input',function(req,res){
-    var hashedString =hash(req,params,input,'this is first hashing');
-    res.send(hashedString);
-});
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
